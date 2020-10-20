63aafde14a817d7d29a21a4656e3fe061082eea5
Kunwoo Yoon <ykw0628@gmail.com>
2020-10-20 14:12:39 -0400

    first commit

---
+++ config/config.php
@@ -0,0 +1,9 @@
+<?php
+ob_start(); // Turns on output buffering
+session_start();
+$timezone = date_default_timezone_set("America/Toronto");
+$con = mysqli_connect("localhost", "root", "4098", "social");
+if(mysqli_connect_errno()){
+    echo "Failed to connect: " . mysqli_connect_errno();
+}
+?>
