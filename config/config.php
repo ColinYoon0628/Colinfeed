81d0416a894d268546c1f2980c6e6dbcdce7a6c7
Kunwoo Yoon <ykw0628@gmail.com>
2020-10-20 17:26:40 -0400

    database set

--- config/config.php
+++ config/config.php
@@ -2,8 +2,9 @@
 ob_start(); // Turns on output buffering
 session_start();
 $timezone = date_default_timezone_set("America/Toronto");
-$con = mysqli_connect("localhost", "root", "4098", "social");
+$con = mysqli_connect("us-cdbr-east-02.cleardb.com", "bfaf8173570296", "05bfd8b8", "heroku_7a6b2cc1468e904");
 if(mysqli_connect_errno()){
     echo "Failed to connect: " . mysqli_connect_errno();
 }
 ?>
+//mysql://bfaf8173570296:05bfd8b8@us-cdbr-east-02.cleardb.com/heroku_7a6b2cc1468e904?reconnect=true
